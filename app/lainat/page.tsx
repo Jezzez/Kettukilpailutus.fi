a90e0ebb785bb451af4fa50599d6204d6f1c2782
Jesez <jesse.kurre@gmail.com>
2026-08-09 18:15:43 +0300

    Update electricity mobile hero

--- app/lainat/page.tsx
+++ app/lainat/page.tsx
@@ -116,7 +116,7 @@ export default function LoansPage() {
             width={712}
             height={993}
             priority
-            className="absolute bottom-0 left-40 h-[92%] w-auto max-w-full object-contain opacity-40"
+            className="absolute bottom-0 left-15 h-[100%] w-auto max-w-full object-contain opacity-40"
             style={{
               WebkitMaskImage:
                 "linear-gradient(to left, #000 84%, transparent 100%)",
